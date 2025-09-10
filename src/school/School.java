ceed7b0967cbf3ffaf144defbdb52583bd6f395c
2389873 <2389873@host.docker.internal>
2025-09-10 11:18:49 -0400

    se

--- src/school/School.java
+++ src/school/School.java
@@ -2,9 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+//https://github.com/Lucas-Dash76/lab3
 package school;
 
-import javafx.application.Application;
+
 
 
 import javafx.application.Application;
@@ -15,6 +16,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+ import java.util.Random;
 /**
  *
  * @author 2389873
@@ -29,11 +31,32 @@ public class School extends Application {
     }
      @Override
     public void start(Stage stage) {
-    Label messageLabel = new Label("Name: ");
-    Button bttn = new Button ("Click me");
-    HBox vb = new HBox(messageLabel, bttn);
-    Scene s = new Scene(vb, 1000, 500);
-    stage.setTitle("first Application");
+    Random random = new Random();
+    BorderPane root = new BorderPane();
+    StackPane middle = new StackPane();
+    Label topLabel = new Label("Random Game");
+    Label bottomLabel = new Label("Waiting... ");
+    Label lblImage = new Label();
+    
+    root.setBottom(bottomLabel);
+    root.setTop(topLabel);
+    
+    int randomNumber = random.nextInt(20) + 1;
+    
+    String number = Integer.toString(randomNumber);
+    Image rndImage;
+    if (randomNumber < 10) {
+        rndImage = new Image("file:Images/10" + number + ".jpg");
+    }
+    else {
+       rndImage = new Image("file:Images/1" + number + ".jpg");
+    }
+    
+    lblImage.setGraphic(new ImageView(rndImage));
+    middle.getChildren().addAll(lblImage);
+    root.setCenter(middle);
+    Scene s = new Scene(root, 250, 300);
+    stage.setTitle("Java Games");
     stage.setScene(s);
     stage.show();
 
